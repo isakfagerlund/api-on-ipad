c1550f2c31ab5ec921eef4badf81c90ff63c0fd1
Isak Fagerlund <fagerlund.isak@gmail.com>
2022-08-31 22:32:54 +0000

    Add family endpoint

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  return res.send('This is with live reloading');
+const fagerlunds = [
+  { name: "Isak", hobby: "Coding" },
+  { name: "Xiao Wen", hobby: "DIY" },
+  { name: "Filou", hobby: "He is a dog"}
+]
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/family", (req, res) => {
+  res.json(fagerlunds);
 });
 
 app.listen("1234", () =>
